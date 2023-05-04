f7126f57fee4a9f927eeae8f4e36fc06b861aae8
Bernardo Conquet <conquetber@gmail.com>
2023-05-04 11:53:16 -0400

    Actualizacion

--- Python Scripts/Angulo Generado Superficies Opticas/Angulo_Generado_Superficies_Opticas.py
+++ Python Scripts/Angulo Generado Superficies Opticas/Angulo_Generado_Superficies_Opticas.py
@@ -1,15 +1,24 @@
 from math import asin, pi
 
-superficie = input("La supeficie es concava o convexa: ")
+superficie = input("La supeficie es concava (1) o convexa (2): ")
 r = float(input("Colocar el radio de curvatura de la superficie (mm): "))
 d = float(input("Colocar el diametro medio de la fresa de diamante (mm): "))
 e = float(input("Colocar el radio de curvatura del filo de la fresa (mm): "))
 
-if superficie == 'concava':
-    alpha = asin(d / (2 * (r - e)))
+x = d / (2 * (r - e))
+
+if x > 1:
+    print("\nPor favor seleccione una fresa de diamante de menor diametro")
+    d = float(input("Colocar el diametro medio de la fresa de diamante (mm): "))
+    e = float(input("Colocar el radio de curvatura del filo de la fresa (mm): "))
+
+x = d / (2 * (r - e))
+
+if superficie == '1':
+    alpha = asin(x)
 else:
-    alpha = asin(d / (2 * (r + e)))
+    alpha = asin(x)
 
 grados = alpha * (180 / pi)
 
-print(f"El angulo de inclinacion de la herramienta es de {alpha:.2f} rad o {grados:.2f}°")
+print(f"\nEl angulo de inclinacion de la herramienta es de {alpha:.2f} rad o {grados:.2f}°")
