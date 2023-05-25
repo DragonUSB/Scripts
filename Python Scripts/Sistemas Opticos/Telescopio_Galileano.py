57cb77b31278d4737c2973a8930d0909ae96ffc2
Bernardo Conquet <conquetber@gmail.com>
2023-05-25 11:29:02 -0400

    Actualizacion

--- Python Scripts/Sistemas Opticos/Telescopio_Galileano.py
+++ Python Scripts/Sistemas Opticos/Telescopio_Galileano.py
@@ -2,9 +2,11 @@ from raytracing import *
 
 def exempleCode(comments = None):
     path = ImagingPath()
-    path.append(Space(d = 35))
+    path.append(Space(d = 100))
+    path.append(ThickLens(R1 = 121, R2 = 1e10, n = 1.55, thickness = 3.14, diameter = 50, label = 'Lens'))
+    path.append(Space(d = 176.15))
     path.append(ThickLens(R1 = 1e10, R2 = 21.21, n = 1.55, thickness = 3.6, diameter = 16.5, label = 'Lens'))
     path.append(Space(d = 25))
-    path.displayWithObject(diameter = 10, comments = comments)
+    path.displayWithObject(diameter = 20, comments = comments)
 
 exempleCode()
